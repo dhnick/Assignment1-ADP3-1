10d64ef5a1d9d7ad09c75d28ae1125bb20ad5a41
jheideman18 <81525864+jheideman18@users.noreply.github.com>
2022-03-24 09:36:13 +0100

    Initial commit

---
+++ src/main/java/za/ac/cput/builder/App.java
@@ -0,0 +1,5 @@
+package za.ac.cput.builder;
+
+// Rename this class to the appropriate entity name and delete this comment
+public class App {
+}
---
+++ src/main/java/za/ac/cput/smallprogram/App.java
@@ -0,0 +1,5 @@
+package za.ac.cput.smallprogram;
+
+// Rename this class to your appropriate program name and delete this comment
+public class App {
+}
