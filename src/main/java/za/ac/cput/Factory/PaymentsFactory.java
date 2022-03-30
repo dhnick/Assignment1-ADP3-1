a2c20d9ed8a41a63e918e77d7312c83d6a9e0ac9
Pints <abongiletshopi@gmail.com>
2022-03-31 01:34:59 +0200

    Domain and factory

---
+++ src/main/java/za/ac/cput/Factory/PaymentsFactory.java
@@ -0,0 +1,25 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.Payments;
+
+import java.util.UUID;
+
+public class PaymentsFactory {
+    public static Payments build(int customerID, int paymentType, int addPayment, String amount, int updatePayment, String cancelPayment, int paymentID){
+        if(customerID<0||paymentType<0||addPayment<0||updatePayment<=0||amount.isEmpty()||cancelPayment.isEmpty()){
+            return null;
+        }
+
+        String tableID= UUID.randomUUID().toString();
+        return new Payments.PaymentsBuilder()
+
+                .setPaymentID(paymentID)
+                .setCustomerID(customerID)
+                .setPaymentType(paymentType)
+                .setAddPayment(addPayment)
+                .setAmount(amount)
+                .setUpdatePayment(updatePayment)
+                .setCancelPayment(cancelPayment)
+                .build();
+    }
+}
